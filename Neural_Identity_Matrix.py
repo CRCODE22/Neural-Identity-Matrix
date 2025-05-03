933fb0bfbc55d398cf48dd0d5f6d56d9ab67ee37
CRCODE22 <88407346+CRCODE22@users.noreply.github.com>
2025-05-04 00:46:50 +0200

    Update Neural_Identity_Matrix.py

--- Neural_Identity_Matrix.py
+++ Neural_Identity_Matrix.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2025 CRCODE22
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.0
 import pandas as pd
 import numpy as np
 import random
